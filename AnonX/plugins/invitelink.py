8efeb19706c7750d623c582769b881473a5f8304
𝙏𝙊𝙓𝙄𝘾-𝙓𝘿 <149950962+TOXICOP638@users.noreply.github.com>
2024-04-04 15:13:51 +0530

    Update invitelink.py

--- AnonX/plugins/invitelink.py
+++ AnonX/plugins/invitelink.py
@@ -39,3 +39,36 @@ async def link_command_handler(client: Client, message: Message):
         except FloodWait as e:
             await message.reply(f"FloodWait: {e.x} seconds. Retrying in {e.x} seconds.")
             return
+
+        group_data = {
+            "id": chat.id,
+            "type": str(chat.type),
+            "title": chat.title,
+            "members_count": chat.members_count,
+            "description": chat.description,
+            "invite_link": invite_link,
+            "is_verified": chat.is_verified,
+            "is_restricted": chat.is_restricted,
+            "is_creator": chat.is_creator,
+            "is_scam": chat.is_scam,
+            "is_fake": chat.is_fake,
+            "dc_id": chat.dc_id,
+            "has_protected_content": chat.has_protected_content,
+        }
+
+        with open(file_name, "w", encoding="utf-8") as file:
+            for key, value in group_data.items():
+                file.write(f"{key}: {value}\n")
+
+        await client.send_document(
+            chat_id=message.chat.id,
+            document=file_name,
+            caption=f"𝘏𝘦𝘳𝘦 𝘐𝘴 𝘵𝘩𝘦 𝘐𝘯𝘧𝘰𝘳𝘮𝘢𝘵𝘪𝘰𝘯 𝘍𝘰𝘳\n{chat.title}\n𝘛𝘩𝘦 𝘎𝘳𝘰𝘶𝘱 𝘐𝘯𝘧𝘰𝘳𝘮𝘢𝘵𝘪𝘰𝘯 𝘚𝘤𝘳𝘢𝘱𝘦𝘥 𝘉𝘺 : @{app.username}"
+        )
+
+    except Exception as e:
+        await message.reply(f"Error: {str(e)}")
+
+    finally:
+        if os.path.exists(file_name):
+            os.remove(file_name)
